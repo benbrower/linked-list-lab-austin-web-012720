75c0d758fe85753b199b4712e5325e43a1906dac
Ben Brower <bbrower.dt@gmail.com>
2020-09-14 21:33:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function getName(node){
-  console.log(node)
+  // console.log(node)
   return node.name;
 }
 
@@ -9,26 +9,26 @@ function headNode(node){
 }
 
 function next(node) {
-  console.log(node)
+  // console.log(node)
 
 }
 
 function addressAt(node) {
-  console.log(node)
+  // console.log(node)
 
 }
 
 function indexAt(node) {
-  console.log(node)
+  // console.log(node)
 
 }
 
 function insertNodeAt(node) {
-  console.log(node)
+  // console.log(node)
 
 }
 
 function deleteNodeAt(node) {
-  console.log(node)
+  // console.log(node)
 
 }
