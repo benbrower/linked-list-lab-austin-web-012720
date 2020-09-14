e25c51978f2b9c00a823671e8acf08db972479bd
Ben Brower <bbrower.dt@gmail.com>
2020-09-14 21:37:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function getName(node){
 function headNode(linkedList, collection){
   console.log('ll', linkedList)
   console.log('col', collection)
-  console.log(collection[linkedlist])
+  console.log(collection[linkedList])
   // return node;
 }
 
